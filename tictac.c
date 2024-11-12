ebab682d3db792bf6bfdb665674c80b6387db43a
JagshanSangha <Jagshansangha@gmail.com>
2024-11-12 02:03:19 -0500

    Update project with latest changes

--- tictac.c
+++ tictac.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <conio.h> //handling console input output 
+
+void tictac(){
+    char a, b;
+    char ch;
+    system("cls");
+    printf("enter two characters:\n");
+    a = getchar();
+    getchar();
+    b = getchar();
+    printf("a=%c b=%c", a, b);
+
+
+
+    char a[9] ={'1','2', '3', '4', '5', '6', '7', '8', '9'};
+    void output(){
+        printf("\n\n\t\t\t---|---|---")
+        printf("\t\t\t---|---|---\n")
+        printf("\t\t\t---|---|---\n")
+        printf("\t\t\t---|---|---\n")
+    }
+}
